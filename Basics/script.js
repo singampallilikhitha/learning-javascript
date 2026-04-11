047dcce926f6916783b9d70c8fa979b4197c799f
Likhitha <likhithasingampalli1212@gmail.com>
2026-04-11 09:27:11 +0530

    upto intro of arrays

---
+++ Basics/script.js
@@ -0,0 +1,69 @@
+// console.log("Likhitha")
+// var a = 10
+// var b = 20
+// if (a > b)
+//     console.log(a)
+// else
+//     console.log(b)
+
+// == -> checks value
+// === -> checks data type also
+// * -> multiple
+// ** -> powers
+
+// var c = "10";
+// var d = 10;
+// if (c == d) console.log(-1);
+// else console.log("Bye");
+// if (c===d) console.log(-1);
+// else console.log("Bye");
+// console.log(c*d);
+// console.log(c**d);
+
+// WAP to print a range(n) of prime numbers
+// var n = 10;
+// for(var i = 1 ; i <= n ; i++){
+//     var fc = 0;
+//     var num = i;
+//     for(var j = 1 ; j <= num ; j++){
+//         if(num%j == 0){
+//             fc++;
+//         }
+//     }
+//     if(fc == 2){
+//         console.log(i);
+//     }
+// }
+// n = 20;
+// console.log(n);
+
+// var x = 20;
+// var x = 40;
+// console.log(x);
+
+// let x = 10;
+// x = 50;
+// console.log(x);
+
+// const x = 50;
+// // x = 80;
+// console.log(x);
+
+// userData = [20,"Hello" , 'bye' , 2.8, false, null];
+// for(let i = 0 ; i < userData.length ; i++)
+//     console.log(userData[i]);
+
+// userData = [20,"Hello" , 'bye' , 2.8, false, null];
+// for(let i = 0 ; i < userData.length ; i++)
+//     console.log(typeof userData[i]);
+
+let s = "100";
+let y = 100;
+// if(s === y)
+if(s==y)
+    console.log("same");
+else
+    console.log("Both are not same");
+console.log(s*y);
+console.log(16**3);
+// console.log(" ==false);

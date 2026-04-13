354975c9ad57a76de8f32fd70e56cafbfc298269
Likhitha <likhithasingampalli1212@gmail.com>
2026-04-13 09:11:59 +0530

    Arrays with objects

--- Basics/script.js
+++ Basics/script.js
@@ -57,19 +57,19 @@
 // for(let i = 0 ; i < userData.length ; i++)
 //     console.log(typeof userData[i]);
 
-let s = "100";
-let y = 100;
-// if(s === y)
-if(s==y)
-    console.log("same");
-else
-    console.log("Both are not same");
-console.log(s*y);
-console.log(16**3);
+// let s = "100";
+// let y = 100;
+// // if(s === y)
+// if(s==y)
+//     console.log("same");
+// else
+//     console.log("Both are not same");
+// console.log(s*y);
+// console.log(16**3);
 // console.log(" ==false);
 
-newArray = [25,79,4,6]
-console.log(newArray);
+// newArray = [25,79,4,6]
+// console.log(newArray);
 // push-> add last
 // pop -> remove last
 // shift-> remove first
@@ -80,4 +80,41 @@ console.log(newArray);
 // newArray.pop();//both are removing last element only
 // newArray.shift();//remove first element
 // newArray.unshift(56);//add in first position
-console.log(newArray);
+// console.log(newArray);
+const obj = {
+    id : 1,
+    name : "Hello"
+}
+const obj1 = {
+    a : [2 , 5 ,6]
+}
+console.log(obj1.a);
+obj.loc = "Hyderabad";
+console.log(obj.id + " " + obj.name + " " + obj.loc);
+console.log(obj);
+//Arrays with objects
+
+Data = [
+    {
+        id : 102,
+        name : "Likhitha",
+        marks : 530
+    },
+    {
+        id : 103,
+        name : "Lotus",
+        marks : 560
+    },
+    {
+        id : 104,
+        name : "Hello",
+        marks : 600
+    }
+]
+console.log(Data);
+console.log(Data[0]);
+console.log(Data[1].name);
+//for of -> arrays
+// for in -> objects
+
+
